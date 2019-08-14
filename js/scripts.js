86f76a07c93ad44d84546118d9f94addd9be5dc5
Cynthia Ponce and Zach Krogh <cynthia@>
2019-08-14 11:06:54 -0700

    change calculator display to div, add jquery front end logic, made button fancy

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,28 @@
+//Business logic:
 var add = function(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert(add(number1, number2));
-
 
 var subtract = function(number1, number2) {
-	return number1 - number2;
+  return number1 - number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert(subtract(number1, number2));
-
-
-var divide = function(number1, number2) {
-	return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert(divide(number1, number2));
 
 var multiply = function(number1, number2) {
-	return number1 * number2;
+  return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-alert(multiply(number1, number2));
+//Front-end logic
+$(document).ready(function (){
+	$("form#add").submit(function(event) {
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("output").text(result);
+
+		event.preventDefault();
+	});
+});
